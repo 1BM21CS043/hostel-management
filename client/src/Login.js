61666130c3bf5c439c9d68a256c5b204e3fd4d6f
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-07-14 00:35:01 +0530

    Frontend and Backend Done

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,41 +1,90 @@
-import React from 'react'
-import "./Login_style.css"
-const Login = ()=>{
-    const redirectHome=()=>{
-        window.location.href="./";
-    }
-    return(
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./Login_style.css";
+
+const Login = () => {
+    const [userId, setUserId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUserIdChange = (event) => {
+        setUserId(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const formData = {
+            usn: userId,
+            password: password
+        };
+
+        axios.post('http://localhost:8080/login', formData)
+            .then(response => {
+                alert("Login Successful.")
+                window.location.href="./"
+                // Handle the response from the server
+                console.log(response.data);
+            })
+            .catch(error => {
+                alert(error.response.data.error)
+                // Handle error if the request fails
+                console.error(error);
+            });
+    };
+
+    const redirectHome = () => {
+        window.location.href = "./";
+    };
+
+    return (
         <div>
-            <div class="hero">
-    <div class="form-box">
-        <div class="button-box">
-            <div id="btn"></div>
-            <button type="button" class="toggle-btn" onclick="Login()">Log In</button>
-            {/* <button type="button" class="toggle-btn" onclick="Register()">Register</button> */}
+            <div className="hero">
+                <div className="form-box">
+                    <div className="button-box">
+                        <div id="btn"></div>
+                        <button type="button" className="toggle-btn" onClick={redirectHome}>Log In</button>
+                        {/* <button type="button" className="toggle-btn" onClick="Register()">Register</button> */}
+                    </div>
+                    <br/><br/><br/>
+                    <div className="social-icons">
+                        <img src="../logo1.png" alt="Logo" />
+                    </div>
+                    <form id="Login" className="input-group" onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            className="input-field"
+                            placeholder="User ID"
+                            required
+                            value={userId}
+                            onChange={handleUserIdChange}
+                        />
+                        <input
+                            type="password"
+                            className="input-field"
+                            placeholder="Enter Password"
+                            required
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                        <input type="checkbox" className="chech-box"/><span> Remember Password</span>
+                        <button type="submit" className="submit-btn">Log In</button>
+                    </form>
+                    <form id="Register" className="input-group">
+                        <input type="int" className="input-field" placeholder="Mobile Number" required/>
+                        <input type="email" className="input-field" placeholder="Email ID" required/>
+                        <input type="text" className="input-field" placeholder="Enter Password" required/>
+                        <input type="text" className="input-field" placeholder="Confirm Password" required/>
+                        <input type="checkbox" className="chech-box"/><span> I agree to the terms & conditions</span>
+                        <button type="submit" className="submit-btn">Register</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <br/><br/><br/>
-        <div class="social-icons">
-            <img src="../logo1.png" />
+    );
+};
 
-        </div>
-        <form id="Login" class="input-group">
-            <input type="text" class="input-field" placeholder="User ID" required/>
-            <input type="text" class="input-field" placeholder="Enter  Password" required/>
-            <input type="checkbox" class="chech-box"/><span> Remember Password</span>
-            <button onClick={redirectHome} type="submit" class="submit-btn">Log In</button>
-        </form>
-        <form id="Register" class="input-group">
-            <input type="int" class="input-field" placeholder="Mobile Number" required/>
-            <input type="email" class="input-field" placeholder="Email ID" required/>
-            <input type="text" class="input-field" placeholder="Enter  Password" required/>
-            <input type="text" class="input-field" placeholder="Confirm  Password" required/>
-            <input type="checkbox" class="chech-box"/><span> I agree to the terms & conditions</span>
-            <button type="submit" class="submit-btn">Register</button>
-        </form>
-    </div>
-   </div>
-        </div>
-    )
-}
-
-export default Login
+export default Login;
--- client/src/Registration.js
+++ client/src/Registration.js
@@ -1,68 +1,136 @@
 import React, { useState } from 'react';
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+import axios from 'axios';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
-//imported library with all countries data
 const Registration = () => {
-    const [message, setMessage] = useState(''); //state for Student's Name
-    const [message2, setMessage2] = useState(''); //state for Gaurdian's Name
-    const [phone, setPhone] = useState(""); //state for phone number
-    const handleChange = event => {
-        const result = event.target.value.replace(/[^a-z]/gi, ''); //regex to accept only characters
-    
-        setMessage(result);
+  const [input_name, setinput_name] = useState('');
+  const [input_usn, setinput_usn] = useState('');
+  const [input_address, setinput_address] = useState('');
+  const [input_guardian_name, setinput_guardian_name] = useState('');
+  const [input_guardian_contact, setinput_guardian_contact] = useState('');
+  const [input_password, setinput_password] = useState('');
+
+  const handleinput_name = event => {
+    const result = event.target.value.replace(/[^a-zA-Z]/g, ''); // Updated regex to allow both lowercase and uppercase letters
+    setinput_name(result);
+  };
+
+  const handleinput_usn = event => {
+    const result = event.target.value.replace(/[^a-zA-Z0-9]/g, ''); // Updated regex to allow both letters and numbers
+    setinput_usn(result);
+  };
+
+  const handleinput_address = event => {
+    const result = event.target.value; // Removed unnecessary regex for address input
+    setinput_address(result);
+  };
+
+  const handleinput_guardian_name = event => {
+    const result = event.target.value.replace(/[^a-zA-Z]/g, ''); // Updated regex to allow both lowercase and uppercase letters
+    setinput_guardian_name(result);
+  };
+
+  const handleinput_guardian_contact = value => {
+    setinput_guardian_contact(value); // Updated to use the value directly from react-phone-input-2
+  };
+
+  const handleinput_password = event => {
+    const result = event.target.value; // Removed unnecessary regex for password input
+    setinput_password(result);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = {
+      name: input_name,
+      usn: input_usn,
+      address: input_address,
+      guardian_name: input_guardian_name,
+      guardian_contact: input_guardian_contact,
+      password: input_password
     };
-    const handleChange2 = event => {
-        const result = event.target.value.replace(/[^a-z]/gi, ''); //regex to accept only characters
-    
-        setMessage2(result);
-    };
-    
-    return (
-        <div className = 'form-container'>
-            <div className = 'registration-form'>
-                <form className = 'form-element'>
-                    <label>
-                        Name:
-                        <input
-        id="message"
-        name="message"
-        type="text"
-        value={message}
-        onChange={handleChange}
-      />
-                    </label>
-                    <label>
-                        USN:
-                        <input type="number" name="usn" />
-                    </label>
-                    <label>
-                        Permanent Address:
-                        <input type="text" name="address" />
-                    </label>
-                    <label>
-                        Guardian's name:
-                        <input
-        id="message2"
-        name="Gaurdian's name"
-        type="text"
-        value={message2}
-        onChange={handleChange2}
-      />
-                    </label>
-                    <label>
-                        Guardian's contact:
-                        <PhoneInput
-          country={'in'}
-          value={phone}
-          onChange={phone => setPhone(phone)}
-        />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        </div>
-    );
+
+    axios
+      .post('http://localhost:8080/signup', formData)
+      .then(response => {
+        // Handle successful response
+        alert(response.data.message);
+        window.location.href="./login"
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Handle error
+        alert(error.response.data.error);
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="form-container">
+      <div className="registration-form">
+        <form className="form-element" onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              id="message"
+              name="message"
+              type="text"
+              value={input_name}
+              onChange={handleinput_name}
+            />
+          </label>
+          <label>
+            USN:
+            <input
+              type="text" // Changed input type to text to allow alphanumeric values
+              name="usn"
+              value={input_usn}
+              onChange={handleinput_usn}
+            />
+          </label>
+          <label>
+            Permanent Address:
+            <input
+              type="text"
+              name="address"
+              value={input_address}
+              onChange={handleinput_address}
+            />
+          </label>
+          <label>
+            Guardian's name:
+            <input
+              id="message2"
+              name="Guardian's name"
+              type="text"
+              value={input_guardian_name}
+              onChange={handleinput_guardian_name}
+            />
+          </label>
+          <label>
+            Guardian's contact:
+            <PhoneInput
+              country={'in'}
+              value={input_guardian_contact}
+              onChange={handleinput_guardian_contact}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              id="message2"
+              name="password"
+              type="password"
+              value={input_password}
+              onChange={handleinput_password}
+            />
+          </label>
+          <input type="submit" value="Submit" className='submitbutton' />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
